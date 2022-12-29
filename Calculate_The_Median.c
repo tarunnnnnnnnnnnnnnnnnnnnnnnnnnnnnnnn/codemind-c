420612803fa3e6281d3cb62fbcc9ce2db8a95fe8
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2022-12-29 11:03:02 +0530

    #include<stdio.h> int main( ) {    int i,j,n,m,a[10],t,h=0;    scanf("%d",&n);    for(i=0;i<n;i++)    {        scanf("%d",&a[i]);    }    for(i=1;i<n;i++)    {       for(j=0;j<n-i;j++)       {          if(a[j]>a[j+1])          {             t=a[j];             a[j]=a[j+1];             a[j+1]=t;          }       }    }    if(n%2!=0)    {       h=(n+1)/2;       m=a[h-1];    }    else    {        h=(n/2);        m=a[h];    } printf("%d",m); }

---
+++ Calculate_The_Median.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main( )
+{
+   int i,j,n,m,a[10],t,h=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=1;i<n;i++)
+   {
+      for(j=0;j<n-i;j++)
+      {
+         if(a[j]>a[j+1]) 
+         {
+            t=a[j];
+            a[j]=a[j+1];
+            a[j+1]=t;
+         }
+      }
+   }
+   if(n%2!=0)
+   {
+      h=(n+1)/2;
+      m=a[h-1];
+   }
+   else
+   {
+       h=(n/2);
+       m=a[h];
+   }
+printf("%d",m);
+}
