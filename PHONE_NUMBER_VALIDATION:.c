378e12630a389760b414e5002b8f4b9ffb7a2730
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-01-28 11:55:13 +0530

    #include<stdio.h> int main() {    long long int a;     scanf("%lld",&a);     if(a>=1000000000 && a<=9999999999)     printf("Valid");     else     {         printf("Invalid");     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   long long int a;
+    scanf("%lld",&a);
+    if(a>=1000000000 && a<=9999999999)
+    printf("Valid");
+    else
+    {
+        printf("Invalid");
+    }
+}
