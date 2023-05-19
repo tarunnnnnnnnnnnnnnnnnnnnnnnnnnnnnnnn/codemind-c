a814667a8f44dc603fdab4c0d8d6f58e288d8617
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-05-19 19:43:35 +0530

    #include <stdio.h> int main() {     int l, r,i;     scanf("%d", &l);     scanf("%d", &r);     for (i = l; i <= r; i++) {         int t = i;         int isD = 1;         while (t != 0) {             int d = t % 10;             if ((d == 0) || (i % d != 0)) {                 isD = 0;                 break;             }             t /= 10;         }         if (isD) {             printf("%d ", i);         }     }    return 0; }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int l, r,i;
+    scanf("%d", &l);
+    scanf("%d", &r);
+    for (i = l; i <= r; i++) {
+        int t = i;
+        int isD = 1;
+        while (t != 0) {
+            int d = t % 10;
+            if ((d == 0) || (i % d != 0)) {
+                isD = 0;
+                break;
+            }
+            t /= 10;
+        }
+        if (isD) {
+            printf("%d ", i);
+        }
+    }
+   return 0;
+}
