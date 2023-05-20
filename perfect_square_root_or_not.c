5399e058a06a5e7f925c05d5de7004bba99f0943
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-05-20 21:03:01 +0530

    #include<stdio.h> int main() {     int i,n,c=0;     scanf("%d", &n);     if(n==1 || n==0)     {         printf("True");         c=1;     }     for(i=2;i<=n/2;i++)     {         if(n==i*i)         {             printf("True");             c=1;             break;         }     }     if(c==0)     printf("False"); }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -3,14 +3,19 @@ int main()
 {
     int i,n,c=0;
     scanf("%d", &n);
-    for(i=1;i<=n/2;i++)
+    if(n==1 || n==0)
+    {
+        printf("True");
+        c=1;
+    }
+    for(i=2;i<=n/2;i++)
     {
         if(n==i*i)
         {
             printf("True");
             c=1;
             break;
-            }
+        }
     }
     if(c==0)
     printf("False");
