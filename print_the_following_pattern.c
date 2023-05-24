9d993452a76c1a25f51150616af5afa9d4bf71b3
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-05-24 15:59:09 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n-2;j++)         {             printf("%d",j);         }             for(int k=n-3;k>=1;k--)             {                 printf("%d",k);             }printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,13 +3,16 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=n;i>=1;i--)
+    for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=i;j++)
+        for(int j=1;j<=n-2;j++)
         {
             printf("%d",j);
         }
-        printf("
+            for(int k=n-3;k>=1;k--)
+            {
+                printf("%d",k);
+            }printf("
 ");
-        }
+    }
 }
